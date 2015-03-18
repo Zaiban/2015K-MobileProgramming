b6baa7e88333e9835828a9747c969e1e6d5c82e6
Zaiban <esaparkkila@gmail.com>
2015-03-18 11:55:27 +0200

    week 11 lecture

--- tampereweather/src/main/java/com/example/tampereweather/WeatherFragment.java
+++ tampereweather/src/main/java/com/example/tampereweather/WeatherFragment.java
@@ -1,5 +1,7 @@
 package com.example.tampereweather;
 
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,6 +13,13 @@ import android.util.Log;
 import android.view.View.OnClickListener;
 
 import com.example.tampereweather.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class WeatherFragment extends Fragment {
@@ -37,11 +46,101 @@ public class WeatherFragment extends Fragment {
         return rootView;
     }
 
+
+
     public void getWeatherData( View view ){
         Log.w("TampereWeather", "in WeatherFragment: GetWeatherData()");
         // Käsittele nappulan painallus
         refreshButton.setText("Getting weather data..");
+
+        // 1. Tee web request
+        new WebTask().execute();
+
+        // 2.
+
+        TextView windText = (TextView) view.findViewById(R.id.windTextView);
+        windText.setText("Wind 500m/s");
+
         TextView temperatureText = (TextView) view.findViewById(R.id.temperatureTextView);
-        temperatureText.setText("jooh");
+        temperatureText.setText("Temperature 235C");
+    }
+
+
+
+    public void doWebRequest() {
+        // These two need to be declared outside the try/catch
+        // so that they can be closed in the finally block.
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        // Will contain the raw JSON response as a string.
+        String forecastJsonStr = null;
+
+        try {
+            // Construct the URL for the OpenWeatherMap query
+            // Possible parameters are avaiable at OWM's forecast API page, at
+            // http://openweathermap.org/API#forecast
+            URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=tampere");
+
+            // Create the request to OpenWeatherMap, and open the connection
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+
+            // Read the input stream into a String
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                return;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return;
+            }
+            forecastJsonStr = buffer.toString();
+            Log.w("TampereWeather", forecastJsonStr);
+        } catch (IOException e) {
+            Log.e("PlaceholderFragment", "Error ", e);
+            // If the code didn't successfully get the weather data, there's no point in attemping
+            // to parse it.
+            return;
+        } finally{
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e("PlaceholderFragment", "Error closing stream", e);
+                }
+            }
+        }
+
+        return;
+    }
+
+    private class WebTask extends AsyncTask {
+        /** The system calls this to perform work in a worker thread and
+         * delivers it the parameters given to AsyncTask.execute() */
+        @Override
+        protected Object doInBackground(Object[] params) {
+            doWebRequest();
+            return null;
+        }
+
+
     }
 }
